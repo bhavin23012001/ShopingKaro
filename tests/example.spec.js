0388b16d508ff8b69d9a1a1800ce502e8b755024
bhavin23012001 <makbhavin01@gmail.com>
2025-07-21 14:10:29 +0530

    hi

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,5 +2,5 @@ const { test, expect } = require('@playwright/test');
 
 test('homepage has title', async ({ page }) => {
     await page.goto('http://localhost:4000'); // Replace with your app's URL
-    await expect(page).toHaveTitle(/Shopping/i); // Adjust this regex
+    await expect(page).toHaveTitle(/Shop/i); // Adjust this regex
 });
