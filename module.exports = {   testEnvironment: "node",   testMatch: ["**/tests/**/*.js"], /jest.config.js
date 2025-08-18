b95903a73e35fb80dbd2d51499992817714cd97c
bhavin23012001 <makbhavin01@gmail.com>
2025-08-18 10:53:28 +0530

    Delete module.exports = {   testEnvironment: "node",   testMatch: ["**/tests/**/*.js"],  directory

--- module.exports = {   testEnvironment: "node",   testMatch: ["**/tests/**/*.js"], /jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: "node",
-  testMatch: ["**/tests/**/*.js"], // only look inside tests folder
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageReporters: ["lcov", "text-summary"]
-};
